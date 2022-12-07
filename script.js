81dc7df9552e63c1dfd23d106426730fe95b888b
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-07 14:46:58 -0800

    Add ability to change grid size

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const button = document.querySelector(".button");
 
 const squareGrid = function (grid) {
 	//Create column
@@ -15,14 +16,17 @@ const squareGrid = function (grid) {
 				.insertAdjacentHTML("afterbegin", "<div class='box'></div>");
 		}
 	}
+
+	const box = document.querySelectorAll(".box");
+	box.forEach((e) =>
+		e.addEventListener("mouseenter", function (e) {
+			e.target.classList.add("color-change");
+		})
+	);
 };
 
-squareGrid(50);
-
-const box = document.querySelectorAll(".box");
-
-box.forEach((e) =>
-	e.addEventListener("mouseenter", function (e) {
-		e.target.classList.add("color-change");
-	})
-);
+button.addEventListener("click", function (e) {
+	let answer = Number(prompt(`Grid Size /n X by X squares`));
+	container.textContent = "";
+	squareGrid(answer);
+});
