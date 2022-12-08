a7327645be35845da2d03e0fd02854e5598aeb7d
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-07 20:20:22 -0800

    Add clear option, Update grid size options

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 
 const container = document.querySelector(".container");
 const button = document.querySelector(".button");
+const buttonClear = document.querySelector(".clear");
+const gridInputRange = document.querySelector(".grid-input-range");
+const gridInputNumber = document.querySelector(".grid-input-number");
+const gridInput = document.querySelector(".grid-number");
+const colorPick = document.querySelector(".color-pick");
 
 const squareGrid = function (grid) {
 	//Create column
@@ -27,8 +32,35 @@ const squareGrid = function (grid) {
 
 squareGrid(30);
 
+// for (let i = 10; i <= 100; i += 10) {
+// 	document
+// 		.querySelector("#tickmarks")
+// 		.insertAdjacentHTML(
+// 			"beforeend",
+// 			`<option value="${i}" label="${i}"></option>`
+// 		);
+// }
+
 // button.addEventListener("click", function (e) {
 // 	let answer = Number(prompt(`Grid Size /n X by X squares`));
 // 	container.textContent = "";
 // 	squareGrid(answer);
 // });
+
+buttonClear.addEventListener("click", function () {
+	console.log("poop");
+	const box = document.querySelectorAll(".box");
+	box.forEach((box) => {
+		box.classList.remove("color-change");
+	});
+});
+
+gridInput.addEventListener("change", function () {
+	container.textContent = "";
+	squareGrid(gridInputRange.value);
+});
+
+gridInput.addEventListener("change", function () {
+	container.textContent = "";
+	squareGrid(gridInputNumber.value);
+});
