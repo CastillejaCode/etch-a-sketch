0c3a60cdd90160960770866963f4a84f0fcfa3d3
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-12 00:01:12 -0800

    Add random color function and additive color function

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ const boxBackgroundColor = [...stylesheet.cssRules].find(
 );
 
 const colorReset = document.querySelector(".color-reset");
+const btnRandom = document.querySelector(".btn-random");
+const btnAdd = document.querySelector(".btn-add");
 
 const squareGrid = function (grid) {
 	//Create column
@@ -46,6 +48,15 @@ const squareGrid = function (grid) {
 
 squareGrid(30);
 
+gridInput.addEventListener("change", function () {
+	container.textContent = "";
+	squareGrid(gridInputRange.value);
+});
+
+gridInput.addEventListener("change", function () {
+	container.textContent = "";
+	squareGrid(gridInputNumber.value);
+});
 // for (let i = 10; i <= 100; i += 10) {
 // 	document
 // 		.querySelector("#tickmarks")
@@ -59,24 +70,17 @@ squareGrid(30);
 // 	let answer = Number(prompt(`Grid Size /n X by X squares`));
 // 	container.textContent = "";
 // 	squareGrid(answer);
+const box = document.querySelectorAll(".box");
 // });
 
+//Reset to default grid
 buttonClear.addEventListener("click", function () {
-	console.log("poop");
 	const box = document.querySelectorAll(".box");
 	box.forEach((box) => {
+		box.style.backgroundColor = `${containerBackgroundColor.style.backgroundColor}`;
+		box.style.backgroundColor = "";
 		box.classList.remove("color-change");
 	});
-});
-
-gridInput.addEventListener("change", function () {
-	container.textContent = "";
-	squareGrid(gridInputRange.value);
-});
-
-gridInput.addEventListener("change", function () {
-	container.textContent = "";
-	squareGrid(gridInputNumber.value);
 });
 
 colorPickBox.addEventListener("input", function () {
@@ -98,4 +102,56 @@ colorReset.addEventListener("click", function () {
 	containerBackgroundColor.style.setProperty("background-color", `white`);
 	colorPickBox.value = "#000000";
 	colorPickBackground.value = "#FFFFFF";
+});
+
+//Random Color on Each Pass Through
+const randomRGB = function () {
+	const random = function () {
+		return Math.floor(Math.random() * 256);
+	};
+	const rgb = `rgb(${random()}, ${random()}, ${random()})`;
+	return rgb;
+};
+
+let state = true;
+
+btnRandom.addEventListener("click", function (e) {
+	const box = document.querySelectorAll(".box");
+	if (state) {
+		box.forEach((box) =>
+			box.addEventListener("mouseenter", function (e) {
+				this.style.backgroundColor = randomRGB();
+			})
+		);
+		state = false;
+	} else {
+		box.forEach((e) =>
+			e.addEventListener("mouseenter", function (e) {
+				this.style.backgroundColor = "";
+			})
+		);
+		state = true;
+	}
+});
+
+let toggle = true;
+btnAdd.addEventListener("click", function () {
+	const box = document.querySelectorAll(".box");
+	if (toggle) {
+		box.forEach((box) => {
+			let i = 0.1;
+			box.addEventListener("mouseenter", function (e) {
+				this.style.backgroundColor = `rgb(0,0,0, ${i})`;
+				i += 0.1;
+			});
+		});
+		toggle = false;
+	} else {
+		box.forEach((e) =>
+			e.addEventListener("mouseenter", function (e) {
+				this.style.backgroundColor = "";
+			})
+		);
+		toggle = true;
+	}
 });
