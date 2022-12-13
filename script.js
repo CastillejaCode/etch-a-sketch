8e28beaac325e7c5533ca0bf8c683a32cd5b9cb6
Julian Krzysiak <jkrzysiak13@gmail.com>
2022-12-12 19:16:11 -0800

    Clean code

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 "use strict";
 
-const container = document.querySelector(".container");
-const button = document.querySelector(".button");
-const buttonClear = document.querySelector(".clear");
+const sketchContainer = document.querySelector(".sketch-container");
+const colorChange = document.querySelectorAll(".color-change");
 
+const gridInput = document.querySelector(".grid-number");
 const gridInputRange = document.querySelector(".grid-input-range");
 const gridInputNumber = document.querySelector(".grid-input-number");
-const gridInput = document.querySelector(".grid-number");
 
 const colorPickBox = document.querySelector("#color");
 const colorPickBackground = document.querySelector("#color-background");
-const colorChange = document.querySelectorAll(".color-change");
+const colorReset = document.querySelector(".color-reset");
 
 const stylesheet = document.styleSheets[1];
 const containerBackgroundColor = [...stylesheet.cssRules].find(
@@ -20,24 +19,26 @@ const boxBackgroundColor = [...stylesheet.cssRules].find(
 	(r) => r.selectorText === ".color-change"
 );
 
-const colorReset = document.querySelector(".color-reset");
 const btnRandom = document.querySelector(".btn-random");
 const btnAdd = document.querySelector(".btn-add");
+const btnClear = document.querySelector(".clear");
 
+//Create Grid
 const squareGrid = function (grid) {
 	//Create column
 	for (let i = 1; i <= grid; i++) {
-		container.insertAdjacentHTML(
+		sketchContainer.insertAdjacentHTML(
 			"afterbegin",
 			"<div class='column-container'></div>"
 		);
+		//Create Rows
 		for (let i = 1; i <= grid; i++) {
 			document
 				.querySelector(".column-container")
 				.insertAdjacentHTML("afterbegin", "<div class='box'></div>");
 		}
 	}
-
+	//Add Color Change
 	const box = document.querySelectorAll(".box");
 	box.forEach((e) =>
 		e.addEventListener("mouseenter", function (e) {
@@ -46,36 +47,21 @@ const squareGrid = function (grid) {
 	);
 };
 
+//Default grid state
 squareGrid(30);
 
+//Change grid size
 gridInput.addEventListener("change", function () {
-	container.textContent = "";
+	sketchContainer.textContent = "";
 	squareGrid(gridInputRange.value);
-});
-
-gridInput.addEventListener("change", function () {
-	container.textContent = "";
+	sketchContainer.textContent = "";
 	squareGrid(gridInputNumber.value);
 });
-// for (let i = 10; i <= 100; i += 10) {
-// 	document
-// 		.querySelector("#tickmarks")
-// 		.insertAdjacentHTML(
-// 			"beforeend",
-// 			`<option value="${i}" label="${i}"></option>`
-// 		);
-// }
 
-// button.addEventListener("click", function (e) {
-// 	let answer = Number(prompt(`Grid Size /n X by X squares`));
-// 	container.textContent = "";
-// 	squareGrid(answer);
-const box = document.querySelectorAll(".box");
-// });
-
-//Reset to default grid
-buttonClear.addEventListener("click", function () {
+//Clear Grid
+btnClear.addEventListener("click", function () {
 	const box = document.querySelectorAll(".box");
+
 	box.forEach((box) => {
 		box.style.backgroundColor = `${containerBackgroundColor.style.backgroundColor}`;
 		box.style.backgroundColor = "";
@@ -83,6 +69,7 @@ buttonClear.addEventListener("click", function () {
 	});
 });
 
+//Change marker color
 colorPickBox.addEventListener("input", function () {
 	boxBackgroundColor.style.setProperty(
 		"background-color",
@@ -90,6 +77,7 @@ colorPickBox.addEventListener("input", function () {
 	);
 });
 
+//Change background color
 colorPickBackground.addEventListener("input", function () {
 	containerBackgroundColor.style.setProperty(
 		"background-color",
@@ -97,6 +85,7 @@ colorPickBackground.addEventListener("input", function () {
 	);
 });
 
+//Back to default colors
 colorReset.addEventListener("click", function () {
 	boxBackgroundColor.style.setProperty("background-color", `black`);
 	containerBackgroundColor.style.setProperty("background-color", `white`);
@@ -113,19 +102,21 @@ const randomRGB = function () {
 	return rgb;
 };
 
-let state = true;
-
+//Random colors
+let stateRandom = true;
 btnRandom.addEventListener("click", function (e) {
 	const box = document.querySelectorAll(".box");
-	if (state) {
+	if (stateRandom) {
+		//Stops both buttons from being active
 		if (btnAdd.classList.contains("button-active")) return;
+
 		btnRandom.classList.toggle("button-active");
 		box.forEach((box) =>
 			box.addEventListener("mouseenter", function (e) {
 				this.style.backgroundColor = randomRGB();
 			})
 		);
-		state = false;
+		stateRandom = false;
 	} else {
 		btnRandom.classList.toggle("button-active");
 		box.forEach((e) =>
@@ -133,28 +124,32 @@ btnRandom.addEventListener("click", function (e) {
 				this.style.backgroundColor = "";
 			})
 		);
-		state = true;
+		stateRandom = true;
 	}
 });
 
-let toggle = true;
+//Additive color, transparent blocks
+let stateAdditive = true;
 btnAdd.addEventListener("click", function () {
 	const box = document.querySelectorAll(".box");
-	if (toggle) {
+	if (stateAdditive) {
+		//Stops both buttons from being active
 		if (btnRandom.classList.contains("button-active")) return;
+
 		btnAdd.classList.toggle("button-active");
 		box.forEach((box) => {
 			let i = 0.1;
-
 			box.addEventListener("mouseenter", function (e) {
 				this.style.backgroundColor = `rgb(0,0,0, ${i})`;
 				i += 0.1;
-				buttonClear.addEventListener("click", function () {
+
+				//Resets first level of transparency on clear
+				btnClear.addEventListener("click", function () {
 					i = 0.1;
 				});
 			});
 		});
-		toggle = false;
+		stateAdditive = false;
 	} else {
 		btnAdd.classList.toggle("button-active");
 		box.forEach((e) =>
@@ -162,6 +157,6 @@ btnAdd.addEventListener("click", function () {
 				this.style.backgroundColor = "";
 			})
 		);
-		toggle = true;
+		stateAdditive = true;
 	}
 });
